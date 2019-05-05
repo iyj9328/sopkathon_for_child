67bb88d5f4456e3836dace682e1d43c56612d8b6
songkyunghee <kyunghee794613@gmail.com>
2019-05-06 07:25:45 +0900

    LoginActivity button 누르면 MainActivity 넘어가는 기능

--- app/src/main/java/sopkathon/com/my/sopkathonforchild/Activity/LoginActivity.kt
+++ app/src/main/java/sopkathon/com/my/sopkathonforchild/Activity/LoginActivity.kt
@@ -1,7 +1,10 @@
 package sopkathon.com.my.sopkathonforchild.Activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import sopkathon.com.my.sopkathonforchild.MainActivity
 import sopkathon.com.my.sopkathonforchild.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +12,14 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        btn_login.setOnClickListener {
+
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
